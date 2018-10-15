b8ef5575541f20681616fabf004c53fa8c0000bc
surhud004 <42063989+surhud004@users.noreply.github.com>
2018-10-15 00:46:41 -0400

    Add files via upload

---
+++ mod2-solution/js/app.js
@@ -0,0 +1,50 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+  toBuy.buy = function(index) {
+    ShoppingListCheckOffService.buyItem(index);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  var toBuyItems = [
+    { name: "cookies", quantity: 10 },
+    { name: "juice", quantity: 2 },
+    { name: "biscuits", quantity: 8 },
+    { name: "chocolates", quantity: 6 },
+    { name: "candies", quantity: 4}];
+
+  var boughtItems = [];
+
+  service.buyItem = function(index) {
+    boughtItems.push(toBuyItems[index]);
+    toBuyItems.splice(index, 1);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
